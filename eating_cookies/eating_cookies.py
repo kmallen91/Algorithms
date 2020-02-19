65ad1abf59b139d1726ee655eb6993b094fdf681
Kevin Allen <kevin.allen91@gmail.com>
2020-02-19 15:12:48 -0800

    working solution for stock prices

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,14 +3,34 @@
 import sys
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
+
+
 def eating_cookies(n, cache=None):
-  pass
+    cookies = [1, 2, 3]
+    # base case if n = 0, return 0
+    if n > 2:
+        return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+    elif n > 1:
+        return eating_cookies(n-2) + eating_cookies(n-1)
+    elif n > 0:
+        return eating_cookies(n-1)
+    else:
+        pass
+    return n
+    # cycle through actions and subtract n by each action
+    # once not divisible by 3, divide by 2, etc, then subtract 1 until 0
+    # increment counter
+    # return counter
+
+
+print(eating_cookies(4))
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
+            ways=eating_cookies(num_cookies), n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,39 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    # iterate through array with pointers
+    a = 0
+    b = 1
+    profit = []
+    for i in range(0, len(prices)-1):
+        while a < (len(prices) - 1) and b < (len(prices)):
+            profit.append(prices[b] - prices[a])
+            b += 1
+        if b > (len(prices) - 1):
+            a += 1
+            i += 1
+            b = a + 1
+        else:
+            pass
+    if len(prices) < 2:
+        return 'List not large enough'
+    return max(profit)
 
 
-if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # find difference between current index and values after
+    # return greatest difference
+list_of_prices = [10, 4, 11, 12, 15]
+print(find_max_profit(list_of_prices))
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+# if __name__ == '__main__':
+#     # This is just some code to accept inputs from the command line
+#     parser = argparse.ArgumentParser(
+#         description='Find max profit from prices.')
+#     parser.add_argument('integers', metavar='N', type=int,
+#                         nargs='+', help='an integer price')
+#     args = parser.parse_args()
+
+#     print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+#         profit=find_max_profit(args.integers), prices=args.integers))
