c2dd68ee9f77ed6102852fedb48edc2cf368209d
Kevin Allen <kevin.allen91@gmail.com>
2020-02-20 14:53:04 -0800

    completed rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,24 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    options = ['rock', 'paper', 'scissors']
+    plays = []
+
+    def plays_rps(num, playlist=[]):
+        if num == 0:
+            return plays.append(playlist)
+        else:
+            for i in range(len(options)):
+                plays_rps(num-1, playlist + [options[i]])
+    plays_rps(n)
+    return plays
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
